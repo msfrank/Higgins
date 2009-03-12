23552ee816a5aa4585e8bd09dfc154c15b84f772
Michael Frank <msfrank@syntaxjockey.com>
2009-03-11 23:07:21 -0700

    added recursive uploading
    upload: added recursive uploading (closes #31)

--- higgins/core/upload.py
+++ higgins/core/upload.py
@@ -2,11 +2,11 @@
 # http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/146306
 
 from sys import argv, exit
-from os import stat
-from os.path import abspath,basename
+from os import stat, walk
+from os.path import abspath, basename, join as pathjoin
 import httplib, urllib
 from mutagen import File
-from twisted.python import usage,log
+from twisted.python import usage, log
 from higgins.logger import Loggable, LEVELS
 
 class UploaderException(Exception):
@@ -19,14 +19,16 @@ class Uploader(object, Loggable):
     domain = 'uploader'
     boundary = '----------ThIs_Is_tHe_bouNdaRY_$'
 
-    def __init__(self, files, host='localhost', port=8000, isLocal=False):
-        self.files = files
+    def __init__(self, paths, host='localhost', port=8000, isLocal=False, recursive=False):
+        self.paths = paths
         self.host = host
         self.log_debug("host: %s" % host)
         self.port = port
         self.log_debug("port: %i" % port)
         self.isLocal = isLocal
         self.log_debug("local-mode: %s" % str(isLocal))
+        self.recursive = recursive
+        self.log_debug("recursive: %s" % str(recursive))
 
     def _get_metadata(self, path):
         metadata = {}
@@ -101,21 +103,29 @@ class Uploader(object, Loggable):
         self.log_debug("server returned status code %s: %s" % (response.status,response.read()))
         return response.status  
         
-    def run(self):
-        failed = []
-        for file in self.files:
-            try:
-                path = abspath(file)
-                metadata = self._get_metadata(path)
-                if self.isLocal:
-                    metadata['local_path'] = path
-                self.log_debug("parsed metadata: %s" % metadata)
-                result = self._upload(path, metadata)
-                self.log_info("uploaded %s" % file)
-            except Exception, e:
-                failed.append((path,e))
-        for file,e in failed:
+    def _process_file(self, path):
+        try:
+            metadata = self._get_metadata(path)
+            if self.isLocal:
+                metadata['local_path'] = path
+            self.log_debug("parsed metadata: %s" % metadata)
+            result = self._upload(path, metadata)
+            self.log_info("uploaded %s" % path)
+        except Exception, e:
             self.log_warning("skipped %s: %s" % (path, e))
+ 
+    def run(self):
+        if self.recursive:
+            for dir in self.paths:
+                for root,dirs,files in walk(dir, topdown=True):
+                    self.log_info("processing directory %s" % root)
+                    for file in files:
+                        file = abspath(pathjoin(root,file))
+                        self._process_file(file)
+        else:
+            for file in self.paths:
+                file = abspath(file)
+                self._process_file(file)
  
 class UploaderObserver(log.DefaultObserver):
     def __init__(self, verbosity=0):
@@ -128,6 +138,7 @@ class UploaderObserver(log.DefaultObserver):
 class UploaderOptions(usage.Options):
     optFlags = [
         ['local-mode', 'l', None],
+        ['recursive', 'r', None],
     ]
     optParameters = [
         ['host', 'h', 'localhost', None],
@@ -148,14 +159,15 @@ class UploaderOptions(usage.Options):
             self['verbose'] = self['verbose'] + 1
     opt_v = opt_verbose
 
-    def parseArgs(self, *files):
-        if len(files) == 0:
+    def parseArgs(self, *paths):
+        if len(paths) == 0:
             raise UploaderException("No media files specified.")
-        self['files'] = files
+        self['paths'] = paths
 
     def opt_help(self):
         import sys
         print "Usage: %s [OPTIONS...] FILE..." % basename(argv[0])
+        print "       %s [OPTIONS...] -r DIR..." % basename(argv[0])
         print ""
         print "  --local-mode,-l        File itself will not be uploaded"
         print "  --host,-h HOST         The host to upload to.  Default is 'localhost'"
@@ -177,7 +189,8 @@ def run_application():
         o.parseOptions(argv[1:])
         observer = UploaderObserver(verbosity=o['verbose'])
         observer.start()
-        uploader = Uploader(o['files'], host=o['host'], port=o['port'], isLocal=o['local-mode'])
+        uploader = Uploader(o['paths'], host=o['host'], port=o['port'],
+            isLocal=o['local-mode'], recursive=o['recursive'])
         uploader.run()
         observer.stop()
         exit(0)
