22df1a5157aa73e53acbf375e2e994b593e81a90
Michael Frank <msfrank@arugess.(none)>
2009-12-07 23:34:06 -0800

    suppressed gstreamer default command help message
    uploader: delayed gstreamer initialization until after command line parsing
    uploader: changed -h to -H, -p to -P

--- higgins/uploader.py
+++ higgins/uploader.py
@@ -5,13 +5,8 @@
 # the COPYING file.
 
 import os, sys
-from higgins.gst import installGstReactor
-installGstReactor()
-from twisted.internet import reactor
 from twisted.python import usage, log
-from higgins.core.upload import Uploader, UploaderException
 from higgins.logger import LOG_FATAL, LOG_WARNING, LOG_DEBUG, LOG_DEBUG2
-from higgins import VERSION
 
 class UploaderObserver(log.DefaultObserver):
     def __init__(self, verbosity=0):
@@ -30,8 +25,8 @@ class UploaderOptions(usage.Options):
         ['recursive', 'r', None],
     ]
     optParameters = [
-        ['host', 'h', 'localhost', None],
-        ['port', 'p', 8000, None, int],
+        ['host', 'H', 'localhost', None],
+        ['port', 'P', 8000, None, int],
     ]
 
     def __init__(self):
@@ -58,23 +53,36 @@ class UploaderOptions(usage.Options):
         print "       %s [OPTIONS...] -r DIR..." % os.path.basename(sys.argv[0])
         print ""
         print "  --local-mode,-l        File itself will not be uploaded"
-        print "  --host,-h HOST         The host to upload to.  Default is 'localhost'"
-        print "  --port,-p PORT         The port higgins is running on.  Default is '8000'"
+        print "  --host,-H HOST         The host to upload to.  Default is 'localhost'"
+        print "  --port,-P PORT         The port higgins is running on.  Default is '8000'"
         print "  --recursive,-r         Recursively upload media files in directories"
-        print "  --help                 Display this help"
-        print "  --version              Display the version"
+        print "  --verbose,-v           Increase verbosity (use -vv or -vvv for even more messages)"
+        print "  --quiet,-q             Decrease verbosity (use -qq for even less messages)"
+        print "  --help,-h              Display this help"
+        print "  --version,-V           Display the version"
         print ""
         sys.exit(0)
+    opt_h = opt_help
 
     def opt_version(self):
+        from higgins import VERSION
         print "Higgins uploader version " + VERSION
         sys.exit(0)
+    opt_V = opt_version
 
 def run_application():
+    exitStatus = 0
     try:
         # parse command line arguments
         o = UploaderOptions()
         o.parseOptions(sys.argv[1:])
+	    # load the gstreamer reactor.  we do this here instead of at the top
+        # of the file because gstreamer seems to implement its own command line
+        # options parsing, and we want to get around that.
+        from higgins.gst import installGstReactor
+        installGstReactor()
+        from twisted.internet import reactor
+        from higgins.core.upload import Uploader
         # create logging observer
         observer = UploaderObserver(verbosity=o['verbose'])
         observer.start()
@@ -83,7 +91,6 @@ def run_application():
             paths = []
             for dir in o['paths']:
                 for root,dirs,files in os.walk(dir, topdown=True):
-                    logger.log_info("processing directory %s" % root)
                     for file in files:
                         paths.append(os.path.abspath(os.path.join(root,file)))
         else:
@@ -96,11 +103,12 @@ def run_application():
         deferred.addCallback(lambda result: reactor.stop())
         reactor.run()
         observer.stop()
-        sys.exit(0)
     except usage.UsageError, e:
         print "Error parsing options: %s" % e
         print ""
         print "Try %s --help for usage information." % os.path.basename(sys.argv[0])
-    except UploaderException, e:
-        print "%s" % e
-    sys.exit(1)
+        exitStatus = 1
+    except Exception, e:
+        print "Error: %s" % e
+        exitStatus = 1
+    sys.exit(exitStatus)
